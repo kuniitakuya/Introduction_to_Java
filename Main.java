0b56fc01b8bc5365942c2b00373cceb78ca26615
國井拓也 <knight92137@gmail.com>
2023-05-06 02:16:46 +0900

    練習問題1-2

--- Main.java
+++ Main.java
@@ -1,40 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    // 1-1 ソースコードの基本構造
-    System.out.println("おはよう");
-    System.out.println("こんにちは");
-    System.out.println("こんばんは");
-    System.out.println("ありがとう");
-
-    // 1-2 変数宣言の文
-    int age;
-    age = 30;
-    System.out.println(age);
-
-    // 1-3 変数の再代入
-    int age2 = 20;
-    System.out.println("私の年齢は" + age2);
-    age2 = 31;
-    System.out.println("いや、本当の年齢は" + age2);
-
-    // 1-4 書き換えてはいけない変数の値を上書きしてしまう
-    double pi = 3.14; //円周率を入れた変数
-    int pie = 5;
-    System.out.println("半径" + pie + "cmのパイの面積は、");
-    System.out.println(pie * pie * pi);
-    System.out.println("パイの半径を倍にします");
-    pi = 10; //誤り。代入すべき変数は「pie」
-    System.out.println("半径" + pie + "cmのパイの面積は、");
-    System.out.println(pie * pie * pi);
-
-    // 1-5 円周率に定数を利用する（エラー）
-    final double PI = 3.14; //定数として円周率を宣言
-    int pie2 = 5;
-    System.out.println("半径" + pie2 + "cmのパイの面積は、");
-    System.out.println(pie2 * pie2 * PI);
-    System.out.println("パイの半径を倍にします");
-    PI = 10; //コンパイルエラーとなり誤りに気づく
-    System.out.println("半径" + pie2 + "cmのパイの面積は、");
-    System.out.println(pie2 * pie2 * PI);
+    // 練習問題１−2
+    int a = 3;
+    int b = 5;
+    int c = a * b;
+    System.out.println("縦幅3横幅5の長方形の面積は、" + c);
   }
 }
