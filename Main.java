f490dc41642e67813bae1d2864f335af38ad1d1a
國井拓也 <knight92137@gmail.com>
2023-05-06 00:48:17 +0900

    1-2 変数宣言の文

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("おはよう");
-    System.out.println("こんにちは");
-    System.out.println("こんばんは");
-    System.out.println("ありがとう");
+    int age;
+    age = 30;
+    System.out.println(age);
   }
 }
