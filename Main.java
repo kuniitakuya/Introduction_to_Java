52122d258f63df6ee29f8fdde7bb453a97e0e04a
國井拓也 <knight92137@gmail.com>
2023-05-06 00:44:14 +0900

    1-1 ソースコードの基本構造

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("おはよう");
+    System.out.println("こんにちは");
+    System.out.println("こんばんは");
+    System.out.println("ありがとう");
+  }
+}
