72c0e43b11cfb7c3baf65d7599bf432e7d3f5031
國井拓也 <knight92137@gmail.com>
2023-05-06 02:00:22 +0900

    1-4 書き換えてはいけない変数の値を上書きしてしまう

--- Main.java
+++ Main.java
@@ -16,5 +16,15 @@ public class Main {
     System.out.println("私の年齢は" + age2);
     age2 = 31;
     System.out.println("いや、本当の年齢は" + age2);
+
+    // 1-4 書き換えてはいけない変数の値を上書きしてしまう
+    double pi = 3.14; //円周率を入れた変数
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+    System.out.println("パイの半径を倍にします");
+    pi = 10; //誤り。代入すべき変数は「pie」
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
   }
 }
