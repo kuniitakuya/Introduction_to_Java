a96c69ef13d592193b0e3758c041e7340ab63582
國井拓也 <knight92137@gmail.com>
2023-05-06 00:56:41 +0900

    1-3 変数の再代入

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
 public class Main {
   public static void main(String[] args) {
+    // 1-1 ソースコードの基本構造
+    System.out.println("おはよう");
+    System.out.println("こんにちは");
+    System.out.println("こんばんは");
+    System.out.println("ありがとう");
+
+    // 1-2 変数宣言の文
     int age;
     age = 30;
     System.out.println(age);
+
+    // 1-3 変数の再代入
+    int age2 = 20;
+    System.out.println("私の年齢は" + age2);
+    age2 = 31;
+    System.out.println("いや、本当の年齢は" + age2);
   }
 }
