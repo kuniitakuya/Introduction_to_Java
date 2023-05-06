7d4c764f3a132f9832f6d921a956e5fe133625e8
國井拓也 <knight92137@gmail.com>
2023-05-06 19:01:33 +0900

    2-1 変数宣言の分と計算の文

---
+++ Part1/Chapter2/Main1.java
@@ -0,0 +1,11 @@
+public class Main1 {
+  public static void main(String[] args) {
+    // 変数宣言の分と計算の文
+    int a;
+    int b;
+    a = 20;
+    b = a + 5;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
