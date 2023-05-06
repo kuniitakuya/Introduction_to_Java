00d504e131025cb085cb73ec6b8338aeb01d7183
國井拓也 <knight92137@gmail.com>
2023-05-06 22:49:57 +0900

    2-5 値より大きな型の変数に代入

---
+++ Part1/Chapter2/Main5.java
@@ -0,0 +1,9 @@
+public class Main5 {
+  // 2-5 値より大きな型の変数に代入
+  public static void main(String[] args) {
+    float f = 3;
+    double d = f;
+    System.out.println(f);
+    System.out.println(d);
+  }
+}
