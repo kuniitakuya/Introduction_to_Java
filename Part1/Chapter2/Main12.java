e834e5ce23308fdcd52b6aa072a8ef5910362de4
國井拓也 <knight92137@gmail.com>
2023-05-07 22:04:31 +0900

    2-12 大きい方の数値を代入する方法

---
+++ Part1/Chapter2/Main12.java
@@ -0,0 +1,9 @@
+public class Main12 {
+  // 2-12 大きい方の数値を代入する方法
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a, b);
+    System.out.println("比較実験：" + a + "と" + b + "とで大きいほうは…" + m);
+  }
+}
