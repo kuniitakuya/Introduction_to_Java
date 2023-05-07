c3dd29764ba4c5642fe5d08dbbac1e9f96551b3a
國井拓也 <knight92137@gmail.com>
2023-05-07 21:35:32 +0900

    2-10 命令実行の分（画面に表示する）

---
+++ Part1/Chapter2/Main10.java
@@ -0,0 +1,9 @@
+public class Main10 {
+  // 2-10 命令実行の分（画面に表示する）
+  public static void main(String[] args) {
+    String name = "すがわら";
+    String message;
+    message = name + "さん、こんにちは";
+    System.out.println(message);
+  }
+}
