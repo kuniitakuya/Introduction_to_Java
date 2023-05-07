a33ae6bc0124ec3ec1447da2f5e29564244f7a09
國井拓也 <knight92137@gmail.com>
2023-05-07 21:50:51 +0900

    2-11 改行なし画面出力の命令

---
+++ Part1/Chapter2/Main11.java
@@ -0,0 +1,9 @@
+public class Main11 {
+  // 2-11 改行なし画面出力の命令
+  public static void main(String[] args) {
+    String name = "すがわら";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+  }
+}
