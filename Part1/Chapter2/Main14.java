7196f76ff75c3cad180587d280700092f4db1343
國井拓也 <knight92137@gmail.com>
2023-05-08 00:45:50 +0900

    2-14 ランダムの数を生成する命令

---
+++ Part1/Chapter2/Main14.java
@@ -0,0 +1,7 @@
+public class Main14 {
+  // 2-14 ランダムの数を生成する命令
+  public static void main(String[] args) {
+    int r = new java.util.Random().nextInt(90);
+    System.out.println("あなたはたぶん、" + r + "歳ですね？");
+  }
+}
