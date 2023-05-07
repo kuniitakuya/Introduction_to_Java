b38a9dfd7bb3fb923df22b5463785002bb009c47
國井拓也 <knight92137@gmail.com>
2023-05-07 22:09:09 +0900

    2-13 String型をint型に変換する命令

---
+++ Part1/Chapter2/Main13.java
@@ -0,0 +1,8 @@
+public class Main13 {
+  // 2-13 String型をint型に変換する命令
+  public static void main(String[] args) {
+    String age = "31";
+    int n = Integer.parseInt(age);
+    System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+  }
+}
