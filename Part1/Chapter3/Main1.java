3e779653a615540bda482e859dd922fcfcfd4da3
國井拓也 <knight92137@gmail.com>
2023-05-08 16:53:48 +0900

    3-1 天気による行動の変化をJavaで表す

---
+++ Part1/Chapter3/Main1.java
@@ -0,0 +1,12 @@
+public class Main1 {
+  // 3-1 天気による行動の変化をJavaで表す
+  public static void main(String[] args) {
+    boolean weather = true;
+    if (weather == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    } else {
+      System.out.println("DVDを見ます");
+    }
+  }
+}
