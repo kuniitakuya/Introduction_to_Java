31ad5ce5bac40530bafc8ff8aac168c5d972c4d8
國井拓也 <knight92137@gmail.com>
2023-05-08 17:16:32 +0900

    3-2 トイレの空きを待つ繰り返し処理

---
+++ Part1/Chapter3/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+  // 3-2 トイレの空きを待つ繰り返し処理
+  public static void main(String[] args) {
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックをする");
+      System.out.println("1分待つ");
+    }
+  }
+}
