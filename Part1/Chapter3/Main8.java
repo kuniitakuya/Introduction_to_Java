6a5231e1119a9a02ee531b22f95fcfb0689e8bb1
國井拓也 <knight92137@gmail.com>
2023-05-10 01:22:49 +0900

    3-8 for文のループ変数iの内容を表示する

---
+++ Part1/Chapter3/Main8.java
@@ -0,0 +1,8 @@
+public class Main8 {
+  // 3-8 for文のループ変数iの内容を表示する
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++) {
+      System.out.print("現在" + (i + 1) + "周目→");
+    }
+  }
+}
