ea73092bf3bc305f2482f0875a99231d35a5ef29
國井拓也 <knight92137@gmail.com>
2023-05-10 22:31:36 +0900

    4-5 配列の要素に値を代入

---
+++ Part1/Chapter4/Main5.java
@@ -0,0 +1,9 @@
+public class Main5 {
+  // 4-5 配列の要素に値を代入
+  public static void main(String[] args) {
+    int[] scores;
+    scores = new int[5];
+    scores[1] = 30;
+    System.out.println(scores[1]);
+  }
+}
