27d2f11307d582d38a4fc0ab80a0c0d724bd942f
國井拓也 <knight92137@gmail.com>
2023-05-10 23:26:17 +0900

    4-14 拡張for文

---
+++ Part1/Chapter4/Main14.java
@@ -0,0 +1,9 @@
+public class Main14 {
+  // 4-14 拡張for文
+  public static void main(String[] args) {
+    int[] scores = { 20, 30, 40, 50, 80 };
+    for (int value : scores) {
+     System.out.println(value);
+    }
+  }
+}
