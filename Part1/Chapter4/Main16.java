09480413c3847c73ba7271ffcdf5223aafe1ad31
國井拓也 <knight92137@gmail.com>
2023-05-10 23:36:29 +0900

    4-16 ガベージコレクション

---
+++ Part1/Chapter4/Main16.java
@@ -0,0 +1,9 @@
+public class Main16 {
+  // 4-16 ガベージコレクション
+  public static void main(String[] args) {
+    boolean judge = true;
+    if (judge == true) {
+      int[] array = { 1, 2, 3 };
+    }
+  }
+}
