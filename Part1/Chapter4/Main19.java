e9413e9bcbbcced5b2302a8c3e42dc5aff44b05b
國井拓也 <knight92137@gmail.com>
2023-05-10 23:54:00 +0900

    4-19 親配列と子配列の要素数を表示

---
+++ Part1/Chapter4/Main19.java
@@ -0,0 +1,8 @@
+public class Main19 {
+  // 4-19 親配列と子配列の要素数を表示
+  public static void main(String[] args) {
+    int[][] scores = { { 40, 50, 60 }, { 80, 60, 70 } };
+    System.out.println(scores.length);
+    System.out.println(scores[0].length);
+  }
+}
