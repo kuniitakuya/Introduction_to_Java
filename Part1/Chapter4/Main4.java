9dc42aa4ec3a9b1531db8296fed473bd8c9db490
國井拓也 <knight92137@gmail.com>
2023-05-10 22:26:49 +0900

    4-4 配列の長さを調べる

---
+++ Part1/Chapter4/Main4.java
@@ -0,0 +1,8 @@
+public class Main4 {
+  // 4-4 配列の長さを調べる
+  public static void main(String[] args) {
+    int[] scores = new int[5];
+    int num = scores.length;
+    System.out.println("要素の数：" + num);
+  }
+}
