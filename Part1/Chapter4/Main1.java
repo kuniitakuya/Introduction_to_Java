7a94f41924eb374ddc5a81245e607b0d54028f8a
國井拓也 <knight92137@gmail.com>
2023-05-10 22:18:43 +0900

    4-1 点数管理プログラム

---
+++ Part1/Chapter4/Main1.java
@@ -0,0 +1,14 @@
+public class Main1 {
+  // 4-1 点数管理プログラム
+  public static void main(String[] args) {
+    int sansu = 20;
+    int kougo = 30;
+    int rika = 40;
+    int eigo = 50;
+    int syakai = 80;
+    int sum = sansu + kougo + rika + eigo + syakai;
+    int avg = sum / 5;
+    System.out.println("合計点：" + sum);
+    System.out.println("平均点：" + avg);
+  }
+}
