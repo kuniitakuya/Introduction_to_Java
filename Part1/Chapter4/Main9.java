d9d4a83e629ef0d9d9399ceb6b557436f29b23b8
國井拓也 <knight92137@gmail.com>
2023-05-10 22:39:11 +0900

    4-9 for文を使って配列を扱う

---
+++ Part1/Chapter4/Main9.java
@@ -0,0 +1,9 @@
+public class Main9 {
+  // 4-9 for文を使って配列を扱う
+  public static void main(String[] args) {
+    int[] scores = { 20, 30, 40, 50, 80 };
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
+    }
+  }
+}
