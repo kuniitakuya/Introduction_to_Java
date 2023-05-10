d8c59814c35c2493af19e6f9b62219a1e527abdc
國井拓也 <knight92137@gmail.com>
2023-05-10 23:50:28 +0900

    4-18 2次元配列の利用

---
+++ Part1/Chapter4/Main18.java
@@ -0,0 +1,13 @@
+public class Main18 {
+  // 4-18 2次元配列の利用
+  public static void main(String[] args) {
+    int[][] scores = new int[2][3];
+    scores[0][0] = 40;
+    scores[0][1] = 50;
+    scores[0][2] = 60;
+    scores[1][0] = 80;
+    scores[1][1] = 60;
+    scores[1][2] = 70;
+    System.out.println(scores[1][1]);
+  }
+}
